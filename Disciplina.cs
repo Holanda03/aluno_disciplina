007a130dea96949337d7e5d965cac5e7bb5d9faf
Holanda03 <carlosabraao@hotmail.com.br>
2023-09-25 14:47:40 -0300

    first commit

---
+++ Disciplina.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlunoDisciplinaExtra
+{
+    internal abstract class Disciplina
+    {
+        protected string nomeDisciplina;
+        protected int cargaHoraria;
+        protected List<Aluno> listaAlunos;
+        protected bool isOfertada;
+
+        public Disciplina(string nomeDisciplina, int cargaHoraria)
+        {
+            this.nomeDisciplina = nomeDisciplina;
+            this.cargaHoraria = cargaHoraria;
+            listaAlunos = new List<Aluno>();
+            isOfertada = false;
+        }
+
+        public int checarQtdAlunos()
+        {
+            var qtdAlunos = listaAlunos.Count;
+
+            if (qtdAlunos >= 12) 
+            {
+                isOfertada = true;
+            } else
+            {
+                isOfertada = false;
+            }
+            
+            return qtdAlunos;
+        }
+    }
+}
---
+++ Ead.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlunoDisciplinaExtra
+{
+    internal class Ead : Disciplina
+    {
+        public Ead(string nomeDisciplina, int cargaHoraria) : base(nomeDisciplina, cargaHoraria)
+        {
+        }
+    }
+}
